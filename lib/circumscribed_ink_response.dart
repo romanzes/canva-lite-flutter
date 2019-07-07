de7789ef1402bbc97329ca3cb81922fa4a625688
Roman Petrenko <roman@canva.com>
2019-07-08 01:19:26 +1000

    circumscribed ink response

---
+++ lib/circumscribed_ink_response.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CircumscribedInkResponse extends StatefulWidget {
+  const CircumscribedInkResponse({
+    Key key,
+    this.child,
+    this.onTap,
+    this.onTapDown,
+    this.onTapCancel,
+    this.onDoubleTap,
+    this.onLongPress,
+    this.onHighlightChanged,
+    this.borderRadius,
+    this.customBorder,
+    this.highlightColor,
+    this.splashColor,
+    this.splashFactory,
+    this.enableFeedback = true,
+    this.excludeFromSemantics = false,
+  });
+
+  final Widget child;
+  final GestureTapCallback onTap;
+  final GestureTapDownCallback onTapDown;
+  final GestureTapCallback onTapCancel;
+  final GestureTapCallback onDoubleTap;
+  final GestureLongPressCallback onLongPress;
+  final ValueChanged<bool> onHighlightChanged;
+  final BorderRadius borderRadius;
+  final ShapeBorder customBorder;
+  final Color highlightColor;
+  final Color splashColor;
+  final InteractiveInkFeatureFactory splashFactory;
+  final bool enableFeedback;
+  final bool excludeFromSemantics;
+
+  @override
+  createState() => _CircumscribedInkResponse();
+}
+
+class _CircumscribedInkResponse extends State<CircumscribedInkResponse> {
+  double _radius = 0;
+
+  Widget build(BuildContext context) {
+    return InkResponse(
+      key: widget.key,
+      child: widget.child,
+      onTap: widget.onTap,
+      onTapDown: widget.onTapDown,
+      onTapCancel: widget.onTapCancel,
+      onDoubleTap: widget.onDoubleTap,
+      onLongPress: widget.onLongPress,
+      onHighlightChanged: widget.onHighlightChanged,
+      containedInkWell: false,
+      highlightShape: BoxShape.circle,
+      radius: _radius,
+      borderRadius: widget.borderRadius,
+      customBorder: widget.customBorder,
+      highlightColor: widget.highlightColor,
+      splashColor: widget.splashColor,
+      splashFactory: widget.splashFactory,
+      enableFeedback: widget.enableFeedback,
+      excludeFromSemantics: widget.excludeFromSemantics,
+    );
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final size = (context.findRenderObject() as RenderBox).size;
+      setState(() {
+        _radius = sqrt(pow(size.width, 2) + pow(size.height, 2)) / 2;
+      });
+    });
+    super.initState();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'circumscribed_ink_response.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -25,64 +27,79 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text(
-                    'Canva Lite',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 34,
+        child: Material(
+          child: Padding(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: Align(
+                    alignment: AlignmentDirectional.centerStart,
+                    child: Text(
+                      'Canva Lite',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 34,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              AspectRatio(
-                aspectRatio: 1.0,
-                child: Material(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                  color: Colors.white,
-                  elevation: 4,
+                AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Material(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Colors.white,
+                    elevation: 4,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Center(
-                        child: InkWell(
-                          child: Text(
-                            'Add',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: CircumscribedInkResponse(
+                          child: Center(
+                            child: Text(
+                              'Add',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
                             ),
                           ),
-                          splashColor: Colors.red,
+                          highlightColor: Colors.transparent,
+                          splashColor: Color(0x1f000000),
+                          splashFactory: InkRipple.splashFactory,
+                          onTap: () {
+                            /* ... */
+                          },
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          'Remove',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
+                      Expanded(
+                        child: CircumscribedInkResponse(
+                          child: Center(
+                            child: Text(
+                              'Remove',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
                           ),
+                          highlightColor: Colors.transparent,
+                          splashColor: Color(0x1f000000),
+                          splashFactory: InkRipple.splashFactory,
+                          onTap: () {
+                            /* ... */
+                          },
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 16),
           ),
-          padding: EdgeInsets.symmetric(horizontal: 16),
         ),
       ),
     );
